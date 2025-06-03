bc3e92d3e6c4e91916a7917bbe9d866297e0bd99
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2025-06-03 18:15:45 +0300

    added preload script

---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,48 @@
+import osUtils from "os-utils";
+import fs from "fs";
+import os from "os";
+import { BrowserWindow } from "electron";
+
+const POLLING_INTERVAL = 500;
+
+export function pollResources(mainWindow: BrowserWindow) {
+  setInterval(async () => {
+    const cpuUsage = await getCpuUsage();
+    const ramUsage = getRamUsage();
+    const { usage: storageUsage } = getStorageData();
+    mainWindow.webContents.send("statistics", {
+      cpuUsage,
+      ramUsage,
+      storageUsage,
+    });
+  }, POLLING_INTERVAL);
+}
+
+function getCpuUsage() {
+  return new Promise((resolve) => osUtils.cpuUsage(resolve));
+}
+
+function getRamUsage() {
+  return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  const stats = fs.statfsSync(process.platform === "win32" ? "C://" : "/");
+  const total = stats.bsize * stats.blocks;
+  const free = stats.bsize * stats.bfree;
+  return {
+    total: Math.floor(total / 1e9),
+    usage: 1 - free / total,
+  };
+}
+
+export function getStaticResourcesData() {
+  const totalStorage = getStorageData().total;
+  const cpuModel = os.cpus()[0].model.trim();
+  const totalMemoryGB = Math.floor(osUtils.totalmem() / 1024);
+  return {
+    totalStorage,
+    cpuModel,
+    totalMemoryGB,
+  };
+}
